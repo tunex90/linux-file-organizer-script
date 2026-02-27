d7b692f726b46342bc0121810198e67ccede054e
tunex90 <tundemosadomi@gmail.com>
2026-02-27 17:53:45 +0000

    script.sh update

---
+++ linux_file_organiser.sh
@@ -0,0 +1,31 @@
+
+3️⃣ File Organizer Script
+
+Objective: Create a script that organizes files in a directory.
+
+Requirements: 
+
+1. Ask user for a directory.
+
+2. Move:
+
+   * `.jpg` → images folder
+   * `.txt` → documents folder
+   * `.log` → logs folder
+
+3. If folder doesn’t exist:
+
+   * Create it automatically
+
+4. Count how many files were moved
+
+5. Print summary at the end
+
+
+Skills Tested: 
+
+* `for` loop
+* Pattern matching (`*.jpg`)
+* `mkdir -p`
+* `mv`
+* Conditional checks
